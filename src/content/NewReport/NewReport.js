70e08609f1f3a7ea69a5f24313f9041cc482cfa0
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-06-14 13:28:03 +0530

    text aread

--- src/content/NewReport/NewReport.js
+++ src/content/NewReport/NewReport.js
@@ -1,98 +1,105 @@
 import { Col } from "@antd";
-import { PasswordInput } from "@carbon/react";
-import { CSSGrid } from "@carbon/react";
-import React from 'react';
+import { CSSGrid, PasswordInput } from "@carbon/react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import {
   Breadcrumb,
   BreadcrumbItem,
+  Button,
   FileUploaderDropContainer,
   Form,
   FormGroup,
   Select,
   SelectItem,
   TextArea,
-  Button,
-} from '@carbon/react';
+} from "@carbon/react";
 
-import Card from '../../components/Card/Card';
+import Card from "../../components/Card/Card";
 
 const NewReport = () => {
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `/`; 
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/`;
     navigate(path);
-  }
+  };
 
   return (
-    <CSSGrid className="newreport-page" fullWidth="true" >
-      <Col lg="16" md="8" sm="4" >
-        <CSSGrid className="newreport-page__banner" >
-          <Col lg="16" md="4" sm="4" >
-            <Breadcrumb noTrailingSlash="true" aria-label="Page navigation" >
+    <CSSGrid className="newreport-page" fullWidth="true">
+      <Col lg="16" md="8" sm="4">
+        <CSSGrid className="newreport-page__banner">
+          <Col lg="16" md="4" sm="4">
+            <Breadcrumb noTrailingSlash="true" aria-label="Page navigation">
               <BreadcrumbItem>
                 <a href="/">Home</a>
               </BreadcrumbItem>
-              <BreadcrumbItem>
-                New Report
-              </BreadcrumbItem>
+              <BreadcrumbItem>New Report</BreadcrumbItem>
             </Breadcrumb>
           </Col>
-          <Col lg="16" md="4" sm="4" >
+          <Col lg="16" md="4" sm="4">
             <h1 className="newreport-page__heading">
-              New <span style={{fontWeight: 'bolder'}}>Witness Report</span>
+              New <span style={{ fontWeight: "bolder" }}>Witness Report</span>
             </h1>
           </Col>
         </CSSGrid>
         <Form>
-          <CSSGrid >
-            <Col lg="10" md="4" sm="4" >
+          <CSSGrid>
+            <Col lg="10" md="4" sm="4">
               <Card className="newreport-page__card">
                 <h4>Incident</h4>
-                <Select id="incident_type" defaultValue="placeholder-item" >
+                <Select id="incident_type" defaultValue="placeholder-item">
                   <SelectItem
                     disabled="true"
                     hidden="true"
                     value="placeholder-item"
                     text="Choose an option"
-                     />
-                  <SelectItem value="po_h" text="Police Harrassment"  />
-                  <SelectItem value="po_b" text="Police Brutality"  />
+                  />
+                  <SelectItem value="po_h" text="Police Harrassment" />
+                  <SelectItem value="po_b" text="Police Brutality" />
                 </Select>
                 <PasswordInput
                   id="case_number"
                   placeholder="Case Number"
                   labelText="Case Number"
-                   />
+                />
                 <PasswordInput
                   id="date"
                   placeholder="Date"
                   labelText="Date"
                   type="text"
-                   />
-                <PasswordInput id="time" labelText="Select a time" type="time"  />
+                />
+                <PasswordInput
+                  id="time"
+                  labelText="Select a time"
+                  type="time"
+                />
                 <TextArea
                   id="incident_description"
                   labelText="Please describe incident"
-                   />
-                <Select id="audio_language" defaultValue="placeholder-item" >
+                />
+                <TextArea
+                  placeholder="Add a reason for this AA"
+                  onChange={(e) => {
+                    this.setState({ reason: e.currentTarget.value });
+                  }}
+                />
+                <Select id="audio_language" defaultValue="placeholder-item">
                   <SelectItem
                     disabled="true"
                     hidden="true"
                     value="placeholder-item"
                     text="Choose an option"
-                     />
-                  <SelectItem value="lang_eng" text="English"  />
-                  <SelectItem value="lang_esp" text="Spanish"  />
-                  <SelectItem value="lang_fr" text="French"  />
-                  <SelectItem value="lang_it" text="Italian"  />
+                  />
+                  <SelectItem value="lang_eng" text="English" />
+                  <SelectItem value="lang_esp" text="Spanish" />
+                  <SelectItem value="lang_fr" text="French" />
+                  <SelectItem value="lang_it" text="Italian" />
                   <SelectItem value="lang_jap" text="Japanese" />
-                  <SelectItem value="lang_sk" text="Korean"  />
+                  <SelectItem value="lang_sk" text="Korean" />
                 </Select>
               </Card>
             </Col>
-            <Col lg="6" md="4" sm="4" >
+            <Col lg="6" md="4" sm="4">
               <Card className="newreport-page__card">
                 <h4>Address</h4>
                 <PasswordInput
@@ -100,50 +107,47 @@ const NewReport = () => {
                   placeholder="Street"
                   labelText="Street"
                   type="text"
-                   />
+                />
                 <PasswordInput
                   id="city"
                   placeholder="City"
                   labelText="City"
                   type="text"
-                   />
+                />
                 <PasswordInput
                   id="state"
                   placeholder="State"
                   labelText="State"
                   type="text"
-                   />
+                />
                 <PasswordInput
                   id="zipcode"
                   placeholder="Zip Code"
                   labelText="Zip Code"
                   type="text"
-                   />
+                />
               </Card>
             </Col>
           </CSSGrid>
-          <CSSGrid className="newreport-page" fullWidth="true" >
-            <Col lg="16" md="8" sm="4" className="newreport-page__r2" >
+          <CSSGrid className="newreport-page" fullWidth="true">
+            <Col lg="16" md="8" sm="4" className="newreport-page__r2">
               <Card className="newreport-page__card">
                 <h4>Upload Files (Audio, Video)</h4>
                 <FormGroup legendText="">
                   <FileUploaderDropContainer
                     id="files"
                     labelText="Drag and drop files here or click to upload"
-                     />
+                  />
                 </FormGroup>
               </Card>
             </Col>
           </CSSGrid>
-          <CSSGrid className="newreport-page__actions" >
-            <Col
-              lg="16"
-              md="8"
-              sm="4"
-              className="newreport-page__save"
-              >
+          <CSSGrid className="newreport-page__actions">
+            <Col lg="16" md="8" sm="4" className="newreport-page__save">
               <Button type="submit">Save</Button>
-              <Button type="button" kind="secondary" onClick={routeChange}>Cancel</Button>
+              <Button type="button" kind="secondary" onClick={routeChange}>
+                Cancel
+              </Button>
             </Col>
           </CSSGrid>
         </Form>
